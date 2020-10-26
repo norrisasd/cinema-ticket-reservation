813cf3d1c55ed55df705a8fe58e4907d3161e2f0
norrisasd <61019363+norrisasd@users.noreply.github.com>
2020-10-26 18:23:43 +0800

    Update conn.php

--- MovieMagic/server/conn.php
+++ MovieMagic/server/conn.php
@@ -1,14 +1,14 @@
 <?php 
     // development connection   
-    // $host="remotemysql.com";
-    // $user="NJmpAKp3n4";
-    // $pass="f7voxHjh3q";
-    // $data="NJmpAKp3n4";
+    $host="remotemysql.com";
+    $user="NJmpAKp3n4";
+    $pass="f7voxHjh3q";
+    $data="NJmpAKp3n4";
     //local
-    $host="localhost";
-    $user="root";
-    $pass="";
-    $data="movie";
+//     $host="localhost";
+//     $user="root";
+//     $pass="";
+//     $data="movie";
     $db = mysqli_connect($host,$user,$pass,$data);
     if (!$db) {
         die("Connection failed: " . mysqli_connect_error());
